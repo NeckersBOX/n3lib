54247afe1ddf68723ce0aab8e55f76b830d16afd
Davide Francesco Merico <hds619@gmail.com>
2018-08-15 17:58:32 +0200

    improving readability

--- src/n3_backward.c
+++ src/n3_backward.c
@@ -6,7 +6,7 @@
 
 struct _n3l_out_to_in {
   N3LData *state;
-  uint64_t dst_l_idx;
+  uint64_t l_idx;
   uint64_t o_idx;
   double delta_w;
 };
@@ -42,7 +42,7 @@ void n3l_backward_propagation(N3LData *state)
   tdata = (struct _n3l_out_to_in *) malloc(state->args->out_size * sizeof(struct _n3l_out_to_in));
   for ( o_idx = 0; o_idx < state->args->out_size; ++o_idx ) {
     tdata[o_idx].state = state;
-    tdata[o_idx].dst_l_idx = state->args->h_layers;
+    tdata[o_idx].l_idx = state->args->h_layers;
     tdata[o_idx].o_idx = o_idx;
     tdata[o_idx].delta_w = n3l_evaluate_out_error(state, o_idx);
 
@@ -66,41 +66,31 @@ void *n3l_execute_backward_propagation(void *arg)
   pthread_t thread;
   struct _n3l_out_to_in *tdata = (struct _n3l_out_to_in *) arg;
   struct _n3l_out_to_in next_tdata;
-  uint64_t src_n_idx;
-  N3L_LMEDIUM_START(tdata->state->args->logger);
+  N3LNeuron *p_n;
+  N3LLogger *p_l = tdata->state->args->logger;
+  uint64_t n_idx;
+  N3L_LMEDIUM_START(p_l);
 
-  N3L_LMEDIUM(tdata->state->args->logger, "Current layer index: %ld", tdata->dst_l_idx);
-  N3L_LMEDIUM(tdata->state->args->logger, "Executing backward propagation with delta: %lf", tdata->delta_w);
+  N3L_LLOW(p_l, "Current layer index: %ld", tdata->l_idx);
+  N3L_LMEDIUM(p_l, "Executing backward propagation with delta: %lf", tdata->delta_w);
 
-  for ( src_n_idx = 0; src_n_idx < tdata->state->net[tdata->dst_l_idx].size; ++src_n_idx ) {
-    if ( tdata->state->net[tdata->dst_l_idx].ltype != N3LInputLayer ) {
+  for ( n_idx = 0; n_idx < tdata->state->net[tdata->l_idx].size; ++n_idx ) {
+    p_n = &tdata->state->net[tdata->l_idx].neurons[n_idx];
+    if ( tdata->state->net[tdata->l_idx].ltype != N3LInputLayer ) {
       next_tdata.state = tdata->state;
-      next_tdata.dst_l_idx = tdata->dst_l_idx - 1;
-      next_tdata.o_idx = src_n_idx;
-      next_tdata.delta_w = tdata->delta_w
-        * tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].weights[tdata->o_idx]
-        * tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].act_prime(
-          tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].result
-        );
+      next_tdata.l_idx = tdata->l_idx - 1;
+      next_tdata.o_idx = n_idx;
+      next_tdata.delta_w = tdata->delta_w * p_n->weights[tdata->o_idx] * p_n->act_prime(p_n->result);
 
-      N3L_LLOW(tdata->state->args->logger,
-        "Neuron(%ld,%ld) - Propagate new delta to previous layer.",
-        tdata->dst_l_idx, src_n_idx);
-
+      N3L_LLOW(p_l, "Neuron(%ld,%ld) - Propagate new delta to previous layer.", tdata->l_idx, n_idx);
       pthread_create(&thread, NULL, n3l_execute_backward_propagation, (void *) &(next_tdata));
       pthread_join(thread, NULL);
     }
 
-    N3L_LPEDANTIC(tdata->state->args->logger, "Weight (%ld,%ld) - Old: %lf", tdata->dst_l_idx, src_n_idx,
-      tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].weights[tdata->o_idx]);
-
-    tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].weights[tdata->o_idx] +=
-      tdata->state->args->learning_rate * tdata->delta_w *
-      tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].input;
-
-    N3L_LMEDIUM(tdata->state->args->logger, "Weight (%ld,%ld) - New: %lf", tdata->dst_l_idx, src_n_idx,
-      tdata->state->net[tdata->dst_l_idx].neurons[src_n_idx].weights[tdata->o_idx]);
+    N3L_LMEDIUM(p_l, "Weight (%ld,%ld) - Old: %lf", tdata->l_idx, n_idx, p_n->weights[tdata->o_idx]);
+    p_n->weights[tdata->o_idx] += tdata->state->args->learning_rate * tdata->delta_w * p_n->input;
+    N3L_LMEDIUM(p_l, "Weight (%ld,%ld) - New: %lf", tdata->l_idx, n_idx, p_n->weights[tdata->o_idx]);
   }
 
-  N3L_LMEDIUM_END(tdata->state->args->logger);
+  N3L_LMEDIUM_END(p_l);
 }
