4656f5b654f09a408bce37ec20114244e61ae8fb
Davide Francesco Merico <hds619@gmail.com>
2018-08-24 01:45:36 +0200

    Fix outputs data malloc

--- src/n3_forward.c
+++ src/n3_forward.c
@@ -36,17 +36,17 @@ double *n3l_forward_propagation(N3LNetwork *net)
 
 double *__n3l_forward_layer(N3LLayer *layer, double *inputs)
 {
-  pthread_t *threads;
-  struct __n3l_forward_data *tdata;
+  pthread_t *threads = NULL;
+  struct __n3l_forward_data *tdata = NULL;
   uint64_t nsize = n3l_neuron_count(layer->nhead), j;
-  N3LNeuron *neuron;
-  double *outputs;
+  N3LNeuron *neuron = NULL;
+  double *outputs = NULL;
 
-  assert(inputs != NULL);
-  
+  assert(inputs != NULL && nsize > 0);
+
   threads = (pthread_t *) malloc(nsize * sizeof(pthread_t));
   for ( neuron = layer->nhead, j = 0; neuron; neuron = neuron->next, ++j ) {
-    if ( !neuron->bias ) {
+    if ( neuron->bias == false ) {
       neuron->input = inputs[j];
     }
 
@@ -58,18 +58,22 @@ double *__n3l_forward_layer(N3LLayer *layer, double *inputs)
   }
   free(threads);
 
-  outputs = (double *) malloc(nsize * sizeof(double));
   if ( layer->type == N3LOutputLayer ) {
+    outputs = (double *) malloc(nsize * sizeof(double));
     for ( neuron = layer->nhead, j = 0; neuron; neuron = neuron->next, ++j ) {
       outputs[j] = neuron->result;
     }
   }
   else {
-    nsize = n3l_neuron_count(layer->next->nhead);
+    assert(layer->next != NULL);
 
+    nsize = n3l_neuron_count(layer->next->nhead);
+    assert(nsize > 0);
+
+    outputs = (double *) malloc(nsize * sizeof(double));
     threads = (pthread_t *) malloc(nsize * sizeof(pthread_t));
     tdata = (struct __n3l_forward_data *) malloc(nsize * sizeof(struct __n3l_forward_data));
-    for ( neuron = layer->nhead, j = 0; neuron; neuron = neuron->next, ++j ) {
+    for ( neuron = layer->next->nhead, j = 0; neuron; neuron = neuron->next, ++j ) {
       tdata[j].ref = neuron->ref;
       tdata[j].layer = layer;
       tdata[j].result = &outputs[j];
@@ -93,6 +97,7 @@ void *__n3l_forward_activate(void *arg)
   N3LNeuron *ref = (N3LNeuron *) arg;
 
   ref->result = ref->act(ref->input);
+
   return NULL;
 }
 
