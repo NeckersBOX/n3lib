048a52349f8dd90cd915ec85e3b8a2a998f305b2
Davide Francesco Merico <hds619@gmail.com>
2018-08-26 14:12:49 +0200

    Add n3_print_net function

--- examples/n3_example.h
+++ examples/n3_example.h
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#define BOOL_STR(b) ((b) ? "True" : "False")
-
 struct n3_example_args {
   bool learning;
   bool mute;
@@ -68,5 +66,40 @@ void n3_example_arguments_parser(int argc, char *argv[], struct n3_example_args 
 
   if ( args->progress ) {
     args->mute = true;
+  }
+}
+
+void n3_print_net(N3LNetwork *net)
+{
+  N3LLayer *layer;
+  N3LNeuron *neuron;
+  N3LWeight *weight;
+  uint64_t size, j, k, z;
+
+  printf("Network\n");
+  printf("+ learning_rate: %lf\n", net->learning_rate);
+  size = n3l_layer_count(net->lhead);
+  printf("- layer_count: %ld\n", size);
+  for ( layer = net->lhead, j = 0; layer; layer = layer->next, ++j ) {
+    printf("- Layer %ld\n", j);
+    printf("  + type: %s\n", layer->type == N3LInputLayer ? "N3LInputLayer" : (layer->type == N3LHiddenLayer ? "N3LHiddenLayer" : (layer->type == N3LOutputLayer ? "N3LOutputLayer" : "Unknown")) );
+    size = n3l_neuron_count(layer->nhead);
+    printf("  - neuron_count: %ld\n", size);
+
+    for ( neuron = layer->nhead, k = 0; neuron; neuron = neuron->next, ++k ) {
+      printf("  - Neuron %ld\n", k);
+      printf("    + act_type: %d\n", (int) neuron->act_type);
+      printf("    + bias: %s\n", neuron->bias ? "true" : "false");
+      printf("    + ref: %ld\n", neuron->ref);
+      printf("    + input: %lf\n", neuron->input);
+
+      size = n3l_neuron_count_weights(neuron->whead);
+      printf("    - weights_count: %ld\n", size);
+      for ( weight = neuron->whead, z = 0; weight; weight = weight->next, ++z ) {
+        printf("    - Weight %ld\n", z);
+        printf("      + value: %lf\n", weight->value);
+        printf("      + target_ref: %ld\n", weight->target_ref);
+      }
+    }
   }
 }
