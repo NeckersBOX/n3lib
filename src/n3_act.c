cf56ab33016295e3d50c2a98c9912209ec927ae4
Davide Merico <davidem@aurigaactive.aurigainformatica.it>
2018-08-29 11:20:54 +0200

    Add act_fun.c file documentation

--- src/n3_act.c
+++ src/n3_act.c
@@ -2,6 +2,8 @@
  * @file n3_act.c
  * @author Davide Francesco Merico
  * @brief This file contains activation functions and their primitive.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Activation_function">Wikipedia - Activation Function</a> 
  */
 #include <assert.h>
 #include <math.h>
@@ -20,96 +22,266 @@
  *
  * Used when no activation function is needed, by default is used for input layer's neurons.
  *
+ *  \f[none(value) = value\f]
+ *
  * @param val input value
  * @return the same value passed as argument.
  *
- * @see n3l_act_identity, n3l_act
+ * @see n3l_act_identity, n3l_act, N3LActType
  */
 double n3l_act_none(double val)
 {
   return val;
 }
 
+/**
+ * @brief Sigmoid activation function.
+ *
+ * \f[sigmoid(value) = \frac{1}{1+ e^{-value}}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_sigmoid_prime, n3l_act, N3LActType
+ */
 double n3l_act_sigmoid(double val)
 {
   return 1 / (1 + exp(-val));
 }
 
+/**
+ * @brief Sigmoid activation primitive function.
+ *
+ * \f[f'(value)= sigmoid(value) * (1 - sigmoid(value))\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_sigmoid, n3l_act_prime, N3LActType
+ */
 double n3l_act_sigmoid_prime(double val)
 {
   return n3l_act_sigmoid(val) * (1 - n3l_act_sigmoid(val));
 }
 
+/**
+ * @brief Tanh activation function.
+ *
+ * \f[tanh(value)= \frac{e^{value}-e^{-value}}{e^{value}+e^{-value}}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_tanh_prime, n3l_act, N3LActType
+ */
 double n3l_act_tanh(double val)
 {
   return tanh(val);
 }
 
+/**
+ * @brief Tanh activation primitive function.
+ *
+ * \f[f'(value) = 1 - tanh(value)^{2}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_tanh, n3l_act_prime, N3LActType
+ */
 double n3l_act_tanh_prime(double val)
 {
   return 1 - pow(tanh(val), 2);
 }
 
+/**
+ * @brief ReLU activation function.
+ *
+ * \f[relu(value) = \begin{cases} 0 & \text{ if } value < 0 \\ value& \text{ if } value \geq 0 \end{cases}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_relu_prime, n3l_act, N3LActType
+ */
 double n3l_act_relu(double val)
 {
   return ( val < 0 ) ? 0 : val;
 }
 
+/**
+ * @brief ReLU activation primitive function.
+ *
+ * \f[(value)= \begin{cases} 0 & \text{ if } value < 0 \\ 1& \text{ if } value \geq 0 \end{cases}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_relu, n3l_act_prime, N3LActType
+ */
 double n3l_act_relu_prime(double val)
 {
   return ( val < 0 ) ? 0 : 1;
 }
 
+/**
+ * @brief Identity activation function.
+ *
+ * \f[identity(value) = value\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_identity_prime, n3l_act_none, n3l_act, N3LActType
+ */
 double n3l_act_identity(double val)
 {
   return val;
 }
 
+/**
+ * @brief Identity activation primitive function.
+ *
+ * \f[f'(value)=1\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_identity, n3l_act_prime, N3LActType
+ */
 double n3l_act_identity_prime(double val)
 {
   return 1;
 }
 
+/**
+ * @brief SoftSign activation function.
+ *
+ * \f[softsign(value)=\frac{value}{1+|value|}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_softsign_prime, n3l_act, N3LActType
+ */
 double n3l_act_softsign(double val)
 {
   return val / (1 + N3L_ABS(val));
 }
 
+/**
+ * @brief SoftSign activation primitive function.
+ *
+ * \f[f'(value)=\frac{1}{(1+|value|)^2}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_softsign, n3l_act_prime, N3LActType
+ */
 double n3l_act_softsign_prime(double val)
 {
   return 1 / pow(1 + N3L_ABS(val), 2);
 }
 
+/**
+ * @brief Leaky ReLU activation function.
+ *
+ * \f[leaky\_relu(value) = \begin{cases} 0.01value & \text{for } value < 0\\ value & \text{for } value \ge 0\end{cases}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_leaky_relu_prime, n3l_act, N3LActType
+ */
 double n3l_act_leaky_relu(double val)
 {
   return val * ((val < 0) ? 0.01 : 1);
 }
 
+/**
+ * @brief Leaky ReLU activation primitive function.
+ *
+ * \f[f'(value) = \begin{cases} 0.01 & \text{for } value < 0\\ 1 & \text{for } value \ge 0\end{cases}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_leaky_relu, n3l_act_prime, N3LActType
+ */
 double n3l_act_leaky_relu_prime(double val)
 {
   return (val < 0) ? 0.01 : 1;
 }
 
+/**
+ * @brief SoftPlus activation function.
+ *
+ * \f[softplus(value) = \ln(1 + e^{value})\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_softplus_prime, n3l_act, N3LActType
+ */
 double n3l_act_softplus(double val)
 {
   return log(1 + exp(val));
 }
 
+/**
+ * @brief SoftPlus activation primitive function.
+ *
+ * \f[f'(value) = \frac{1}{1+ e^{-value}}\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_softplus, n3l_act_prime, N3LActType
+ */
 double n3l_act_softplus_prime(double val)
 {
   return 1 / (1 + exp(-val));
 }
 
+/**
+ * @brief Swish activation function.
+ *
+ * \f[swish(value)=value * sigmoid(value)\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_swish_prime, n3l_act, N3LActType
+ */
 double n3l_act_swish(double val)
 {
   return val * n3l_act_sigmoid(val);
 }
 
+/**
+ * @brief Swish activation primitive function.
+ *
+ * \f[f'(value)=swish(value) + sigmoid(value) * (1 - swish(value))\f]
+ *
+ * @param val input value
+ * @return the results from the formula above.
+ *
+ * @see n3l_act_swish, n3l_act_prime, N3LActType
+ */
 double n3l_act_swish_prime(double val)
 {
   return n3l_act_swish(val) + n3l_act_sigmoid(val) * (1 - n3l_act_swish(val));
 }
 
+/**
+ * @brief Get the pointer to an activation function.
+ *
+ * @param type Activation function type.
+ * @return Pointer to the function chosen.
+ *
+ * @see n3l_act_prime, N3LActType
+ */
 N3LAct n3l_act(N3LActType type)
 {
   switch(type) {
@@ -128,6 +300,14 @@ N3LAct n3l_act(N3LActType type)
   assert("Unmanaged N3LActType" && 0);
 }
 
+/**
+ * @brief Get the pointer to an activation primitive function.
+ *
+ * @param type Activation function type.
+ * @return Pointer to the function's primitive chosen.
+ *
+ * @see n3l_act, N3LActType
+ */
 N3LAct n3l_act_prime(N3LActType type)
 {
   switch(type) {
